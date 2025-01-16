f61ce85b88d914fa9f2e75edbcf0bf03a60f47d2
Racheal <rachealnjugush@gmail.com>
2025-01-16 14:23:05 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_migrate import Migrate
 
 # contains definitions of tables and associated schema constructs
 metadata = MetaData()
@@ -19,3 +20,14 @@ class Employee(db.Model):
 
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.salary}>'
+    
+class Department(db.Model):
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Department {self.id}, {self.name}, {self.location}>'
+
